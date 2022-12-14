799054da9727ff4d988b583d383f2ee005ecfee9
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-12-14 13:35:14 +0100

    global

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "background-color": "#f5c330",
+      /* PRIMÆRE */
+
+      sol: "#f5c330",
+      rust: "#d2682c",
+      himmel: "#cdeff6",
+      regnorm: "#d5bba4",
+      graes: "#656a41",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
