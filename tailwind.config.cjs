bdba470c54baf19440fe0421944277fac33c8885
Martha Scales <mart1c05@stud.kea.dk>
2022-12-14 13:55:49 +0100

    push

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -53,43 +53,43 @@ module.exports = {
   /* sol */
 
   "sol-100": "#fdf8d9",
-  "sol-200": #fbf0b4;
-  --sol-300: #f8e48f;
-  --sol-400: #f3d774;
-  --sol-500: #edc551;
-  --sol-600: #caa340;
-  --sol-700: #a98332;
-  --sol-800: #886524;
-  --sol-900: #6f501b;
+  // "sol-200": #fbf0b4;
+  // --sol-300: #f8e48f;
+  // --sol-400: #f3d774;
+  // --sol-500: #edc551;
+  // --sol-600: #caa340;
+  // --sol-700: #a98332;
+  // --sol-800: #886524;
+  // --sol-900: #6f501b;
 
-  /* regnorm */
+  // /* regnorm */
 
-  --regnorm-100: #fcf9f2;
-  --regnorm-200: #f9f2e5;
-  --regnorm-300: #f0e4d2;
-  --regnorm-400: #e2d3c0;
-  --regnorm-500: #d1bca7;
-  --regnorm-600: #b2937b;
-  --regnorm-700: #926e56;
-  --regnorm-800: #744c38;
-  --regnorm-900: #5f3423;
+  // --regnorm-100: #fcf9f2;
+  // --regnorm-200: #f9f2e5;
+  // --regnorm-300: #f0e4d2;
+  // --regnorm-400: #e2d3c0;
+  // --regnorm-500: #d1bca7;
+  // --regnorm-600: #b2937b;
+  // --regnorm-700: #926e56;
+  // --regnorm-800: #744c38;
+  // --regnorm-900: #5f3423;
 
-  /* COLOR | ALIAS */
+  // /* COLOR | ALIAS */
 
-  /* BACKGROUNDS */
+  // /* BACKGROUNDS */
 
-  --background-color: #fffdf9;
-  --banner-background: #282828;
-  --footer-background1: #484848;
-  --footer-background2: #323232;
+  // --background-color: #fffdf9;
+  // --banner-background: #282828;
+  // --footer-background1: #484848;
+  // --footer-background2: #323232;
 
-  /* HEADINGS */
+  // /* HEADINGS */
 
-  --color-headings: #000000;
-  --color-landingpage-headings: var(--sol-300);
-  --color-abonnement-headings: var(--sol-500);
-  --color-blomster-headings: var(--himmel-500);
-  --color-banner-heading: var(--sol-300);
+  // --color-headings: #000000;
+  // --color-landingpage-headings: var(--sol-300);
+  // --color-abonnement-headings: var(--sol-500);
+  // --color-blomster-headings: var(--himmel-500);
+  // --color-banner-heading: var(--sol-300);
 
     },
     
